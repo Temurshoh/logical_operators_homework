3feaee8575abaa62a6954d3248347a25df01c502
Temurshoh <turdimurodovtemur2@gamil.com>
2022-12-08 20:23:56 +0500

    18

--- logic18.py
+++ logic18.py
@@ -9,7 +9,8 @@ def main(a):
     x2=a//10%10
     x3=a//100%10
     x4=a//1000%10
-    x5=a%10000
-    return x5<x4<x3<x2<x1
+    x5=a//10000
+    print(x5,x4,x3,x2,x1)
+    return x5<x4 and x4<x3 and x3<x2 and x2<x1
 print(main(12347))
    
