6de3ba1c9d31f35f6c8f110e8aef1aed9d05596b
Temurshoh <turdimurodovtemur2@gamil.com>
2022-12-11 09:12:32 +0500

    20

--- logic20.py
+++ logic20.py
@@ -8,8 +8,40 @@ def main(n):
     Returns:
         bool: answer
     """
-    x1=n//10000
-    return x1==1 and n>=10000
-print(main(1100))
+    tim_1=0
+    tim_0=0
+
+    x1=n%10
+    n//=10
+    print(n)
+    tim_1+=x1==1
+    tim_0+=x1==0 and n!=1
+
+    x2=n%10
+    n//=10
+    print(n)
+    tim_0+=x2==0 and n!=1
+    tim_1+=x2==1
+
+    x3=n%10
+    n//=10
+    print(n)
+    tim_1+=x3==1
+    tim_0+=x3==0 and n!=1
+    
+    x4=n%10
+    n//=10
+    print(n)
+    tim_1+=x4==1
+    tim_0+=x4==0 and n!=1
+
+    x5=n%10
+    n//=10
+    print(n)
+    tim_1+=x5==1
+    tim_0+=x5==0 and n!=1
+    
+    return tim_1>tim_0
+print(main(110))
 print(main(11001))
 
