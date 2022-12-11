6d42043ba8051bc322ab1078815aea801bd6b711
Temurshoh <turdimurodovtemur2@gamil.com>
2022-12-11 09:14:08 +0500

    20

--- logic20.py
+++ logic20.py
@@ -15,31 +15,31 @@ def main(n):
     n//=10
     print(n)
     tim_1+=x1==1
-    tim_0+=x1==0 and n!=1
+    tim_0+=x1==0 and n>=1
 
     x2=n%10
     n//=10
     print(n)
-    tim_0+=x2==0 and n!=1
+    tim_0+=x2==0 and n>=1
     tim_1+=x2==1
 
     x3=n%10
     n//=10
     print(n)
     tim_1+=x3==1
-    tim_0+=x3==0 and n!=1
+    tim_0+=x3==0 and n>=1
     
     x4=n%10
     n//=10
     print(n)
     tim_1+=x4==1
-    tim_0+=x4==0 and n!=1
+    tim_0+=x4==0 and n>=1
 
     x5=n%10
     n//=10
     print(n)
     tim_1+=x5==1
-    tim_0+=x5==0 and n!=1
+    tim_0+=x5==0 and n>=1
     
     return tim_1>tim_0
 print(main(110))
